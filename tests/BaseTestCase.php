f3c56940eb0227f70690750556f95f0b946e94a7
RomaGilyov <rgilyov@gmail.com>
2018-06-20 12:04:52 +0300

    file manager

---
+++ tests/BaseTestCase.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace RGilyov\FileManage\Test;
+
+use Orchestra\Testbench\TestCase;
+use RGilyov\FileManager\Models\File;
+use RGilyov\FileManager\Models\Media;
+use RGilyov\FileManager\Models\Video;
+use RGilyov\Providers\FileManagerServiceProvider;
+
+/**
+ * Class BaseTestCase
+ * @package RGilyov\FileManage\Test
+ */
+abstract class BaseTestCase extends TestCase
+{
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            FileManagerServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'sqlite');
+    }
+
+    /**
+     * @return void
+     */
+    public function tearDown()
+    {
+        Media::all()->each(function (Media $model) {
+            $model->delete();
+        });
+
+        Video::all()->each(function (Video $model) {
+            $model->delete();
+        });
+
+        File::all()->each(function (File $model) {
+            $model->delete();
+        });
+
+        parent::tearDown();
+    }
+}
---
+++ tests/FileManagerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace RGilyov\CsvImporter\Test;
+
+use RGilyov\FileManage\Test\BaseTestCase;
+
+/**
+ * Class FileManagerTest
+ * @package RGilyov\CsvImporter\Test
+ */
+class FileManagerTest extends BaseTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+
+    }
+
+    /** @test */
+    public function it_can()
+    {
+
+    }
+}
